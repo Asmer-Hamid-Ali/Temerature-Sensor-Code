6dd1b10a3908e055719e35cf3c2533cac257ace5
Asmer-Hamid-Ali <51397585+Asmer-Hamid-Ali@users.noreply.github.com>
2020-05-16 10:25:32 +0530

    Update Reading_from_temperature_sensor.ino

--- Reading_from_temperature_sensor.ino
+++ Reading_from_temperature_sensor.ino
@@ -1,35 +1,57 @@
+const char releaseNumber[6] = "1.10";              // Displays the release on the menu
+
+
+// Declare temperature variable here
 float temp=0;
-int tempPin = 0;
 float prev_temp=0;
+
+// Declare all pins here
+int tempPin = A0;
+
 void setup() 
 {
-   Serial.begin(9600);
+  //   Initialise the sensor here. 
+  Serial.begin(9600);
+  temp = analogRead(tempPin) * 0.48828125;
+  prev_temp=temp;
+  
 }
 
 void loop() 
 {
-//   temp = analogRead(tempPin);
    prev_temp=temp;
-   Serial.print(prev_temp);
-   temp = analogRead(tempPin);
-   // read analog volt from sensor and save to variable temp
-   temp = temp * 0.48828125;
-   ReadTemperature();
+   Serial.println(prev_temp);
+   temp = analogRead(tempPin)*0.48828125;
+   ReadTemperature(prev_temp,temp);
+
 }
 void myDelay(int del) 
 {
   unsigned long myPrevMillis = millis();
   while (millis()- myPrevMillis <= del);
 }
-void ReadTemperature()
+
+/*
+   This will check the absolute value of current and previousTemperature. 
+   If it crosses the threshold then it will return 1 else 0. 
+ */
+void ReadTemperature(float a,float b)
 {
-  if abs((prev_temp-temp) >=1)
+  if abs(a-b>=1.0)
    {
-   // convert the analog volt to its temperature equivalent
-   Serial.print("TEMPERATURE = ");
-   Serial.print(temp); // display temperature value
-   Serial.print("*C");
-   Serial.println();
-   myDelay(100); // update sensor reading each one second
+     // convert the analog volt to its temperature equivalent
+     Serial.print("TEMPERATURE = ");
+     Serial.print(b); // display temperature value
+     Serial.print("*C");
+     Serial.println();
+     myDelay(100); // update sensor reading each one second
    }
+  else
+  {
+     Serial.print("TEMPERATURE = ");
+     Serial.print(a); // display temperature value
+     Serial.print("*C");
+     Serial.println();
+     myDelay(100); // update sensor reading each one second
+  }
 }
